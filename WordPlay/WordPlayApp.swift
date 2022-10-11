9b293c3a576ad0e7d6baad6c25c6b155100b065e
Nebo <kindersmail08@gmail.com>
2022-10-12 02:21:13 +0300

    init commit

---
+++ WordPlay/WordPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordPlayApp.swift
+//  WordPlay
+//
+//  Created by Nebo on 12.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
